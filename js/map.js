f90eaf64b2eb60894f9bbecc32c9daafb5ef02f2
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-02-16 16:37:11 -0600

    Added basic JS for map.

---
+++ js/map.js
@@ -0,0 +1,26 @@
+/**
+ * Main JS for Map
+ */
+
+(function($) {
+  $(document).ready(function() {
+    var url = 'http://a.tiles.mapbox.com/v3/nps.map-qhh5espx.jsonp';
+
+    // Create map
+    var mm = com.modestmaps;
+    wax.tilejson(url, function(tilejson) {
+      var m = new mm.Map('map-container', new wax.mm.connector(tilejson));
+      
+      // Center
+      //m.setCenterZoom(new mm.Location(39, -98), 2);
+      m.setCenterZoom(new mm.Location(tilejson.center[1], tilejson.center[0]), 6);
+      
+      // Controls and interactions
+      wax.mm.zoomer(m, tilejson).appendTo(m.parent);
+      wax.mm.legend(m, tilejson).appendTo(m.parent);
+      wax.mm.interaction(m, tilejson);
+      wax.mm.attribution(m, tilejson).appendTo(m.parent);
+    });
+
+  });
+})(jQuery);
